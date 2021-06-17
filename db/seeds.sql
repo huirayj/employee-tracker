7db390a862f130437237ff2c1ad33b68a38ca4c0
Justin Hui <huirayj@gmail.com>
2021-06-17 16:35:31 -0400

    updated seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,8 +4,7 @@ VALUES
     ('R & D'),
     ('Manufacturering'),
     ('Finance'),
-    ('Marketing'),
-    ('Customer Service');
+    ('Marketing');
 
 INSERT INTO role 
     (title, salary, department_id)
@@ -13,19 +12,19 @@ VALUES
    ("CEO", 1400000, null),
    ("Researcher", 70000, 1),
    ("Engineer", 75000, 2),
-   ("Accountant", 80000,3),
+   ("Accountant", 80000, 3),
    ("Sales Manager", 70000, 4),
-   ("Representative", 40000, 5);
+   ("Representative", 40000, 4);
 
-INSERT INTO employees 
+INSERT INTO employee 
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Marcus", "Fenix", 4, 1),
-    ("Johnny", "Cage", 5, 4),
+    ("Arthas", "Menethil", 1, null),
     ("Samuel", "Oak", 2, 1),
-    ("Carmen", "Sandiego", 5, 3),
+    ("Gordon", "Freeman", 3, 1),
+    ("Marcus", "Fenix", 4, 1),
+    ("Johnny", "Cage", 5, 1),
+    ("Carmen", "Sandiego", 6, 5),
     ("Nathan", "Drake", 6, 5),
     ("Jill", "Valentine", 6, 5),
-    ("Niko", "Bellic", 6, 5),
-    ("Gordon", "Freeman", 3, 2),
-    ("Arthas", "Menethil", 1, null);
+    ("Niko", "Bellic", 6, 5);
