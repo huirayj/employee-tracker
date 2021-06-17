17ec940831c6143bed563b5f3c9b16e58e0a228b
Justin Hui <huirayj@gmail.com>
2021-06-17 09:21:36 -0400

    added more employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,17 +9,23 @@ VALUES
 
 INSERT INTO role 
     (title, salary, department_id)
-VALUES 
-   ("Researcher", 70000, 1),
-   ("Engineer", 75000, 2),
-   ("Accountant", 80000, 3),
-   ("Sales Manager", 70000, 4),
-   ("Representative", 40000, 5);
+VALUES
+   ("CEO", 1400000, 1),
+   ("Researcher", 70000, 2),
+   ("Engineer", 75000, 3),
+   ("Accountant", 80000, 4),
+   ("Sales Manager", 70000, 5),
+   ("Representative", 40000, 6);
 
 INSERT INTO employees 
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Marcus", "Fenix", 3, null),
-    ("Johnny", "Cage", 4, 3),
-    ("Samuel", "Oak", 1, 1),
-    ("Carmen", "Sandiego", 4, 2);
+    ("Marcus", "Fenix", 4, 1),
+    ("Johnny", "Cage", 5, 4),
+    ("Samuel", "Oak", 2, 1),
+    ("Carmen", "Sandiego", 5, 3),
+    ("Nathan", "Drake", 6, 5),
+    ("Jill", "Valentine", 6, 5),
+    ("Niko", "Bellic", 6, 5),
+    ("Gordon", "Freeman", 3, 2),
+    ("Arthas", "Menethil", 2, null);
